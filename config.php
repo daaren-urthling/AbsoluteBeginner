ea1111cf050436d41c900eac02ee07cd1abad7d1
Daaren Urthling <daaren.urthling@hotmail.com>
2022-01-16 20:41:03 +0100

    yet other tests with relative paths

--- config.php
+++ config.php
@@ -1,14 +1,21 @@
 <?php 
 	session_start();
 
-	define('BASE_URL', '/absolute-beginner/');
-	if (!empty(getenv("ROOT")))
-	{
-		define ('ROOT', getenv("ROOT"));
-	}
-	else
-	{
-		define ('ROOT', realpath(dirname(__FILE__)));
-	}
+	
 
+	echo 'DOCUMENT_ROOT ' . $_SERVER['DOCUMENT_ROOT'] . "</br>";
+	echo 'SERVER_NAME ' . $_SERVER['SERVER_NAME'] . "</br>";
+	echo 'HTTP_REFERER ' . $_SERVER['HTTP_REFERER'] . "</br>";
+	echo 'HTTP_HOST ' . $_SERVER['HTTP_HOST'] . "</br>";
+	echo "SERVER_NAME +  REQUEST_URI http://" . $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI']. "</br>";
+    if(isset($_SERVER['HTTPS'])){
+        $protocol = ($_SERVER['HTTPS'] && $_SERVER['HTTPS'] != "off") ? "https" : "http";
+    }
+    else{
+        $protocol = 'http';
+    }
+	echo $protocol . "://".$_SERVER['SERVER_NAME'].dirname($_SERVER["REQUEST_URI"].'?').'/' . "</br>";
+
+	define ('ROOT', realpath(dirname(__FILE__)));
+	define('BASE_URL', $protocol . "://".$_SERVER['SERVER_NAME'].dirname($_SERVER["REQUEST_URI"].'?').'/');
 ?>
